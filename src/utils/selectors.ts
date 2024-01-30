270fed8da911b10eb8acfa8d172f96dfc348cecb
Manuel Casares <manuel.casares@mdevz.com>
2024-01-30 18:51:52 -0300

    chore: change contact selector

--- src/utils/selectors.ts
+++ src/utils/selectors.ts
@@ -1,4 +1,4 @@
 import { ICliente } from 'types';
 
 export const getChatTabSelector = (cliente: ICliente, contactBaseName: string) =>
-  `div[role="gridcell"] span[title="${contactBaseName} ${cliente.numero_identificador}"]`;
+  `div[role="gridcell"] span[title="TELECENTRO ${contactBaseName} ${cliente.numero_identificador}"]`;
