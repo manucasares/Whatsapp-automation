46f3bc00492bc7eeeb2dfc2f3a9c1cdee8d03ff6
Manuel Casares <manuel.casares@mdevz.com>
2024-04-29 20:55:25 -0300

    fix: enabling args on puppeteer lunch

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -6,7 +6,7 @@ export const WHATSAPP_URL = 'https://web.whatsapp.com/';
 export const STREET_SEPARATOR = '#';
 export const LAUNCH_CONFIG: PuppeteerLaunchOptions = {
   headless: false,
-  // args: ['--user-data-dir=./Google/Chrome/User Data/'],
+  args: ['--user-data-dir=./Google/Chrome/User Data/', '--disable-extensions'],
   // devtools: true,
   // slowMo: 1000,
   dumpio: true, // Show logs in Chrome
