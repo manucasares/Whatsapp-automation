bac3c7c6e05569a0fd9a56264df317d1b8d446e4
Manuel Casares <manuel.casares@mdevz.com>
2024-05-11 13:05:39 -0300

    fix: changed chat_input selector

--- src/constants/selectors.ts
+++ src/constants/selectors.ts
@@ -1,3 +1,3 @@
-export const CHAT_INPUT_SELECTOR = 'div > div[title="Escribe un mensaje"]';
+export const CHAT_INPUT_SELECTOR = 'div > div[aria-label="Escribe un mensaje"]';
 export const SIDEBAR_SEARCH_INPUT_SELECTOR = "div#side [role='textbox']";
 export const MESSAGES_SPINNER_SELECTOR = "div[title='cargando mensajes...']";
